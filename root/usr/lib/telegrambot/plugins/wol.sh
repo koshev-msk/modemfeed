1387e19b5015394c236362dae895c099fe9253f5
Konstantine Shevlakov <konst.shell@gmail.com>
2021-02-13 10:07:23 -0500

    telegrambot: plugins: wol: fixed <F2>telegrambot: plugins: wol: fixed <F2>

--- root/usr/lib/telegrambot/plugins/wol.sh
+++ root/usr/lib/telegrambot/plugins/wol.sh
@@ -1,7 +1,7 @@
-#!.bin/sh
+#!/bin/sh
 WOLIFACE=$1
 HWADDR=$2
-if [ -z $WOLIFACE] && [ -z "$HWADDR" ]; then
+if [ -z $WOLIFACE ]  && [ -z "$HWADDR" ]; then
 	echo -en "Usage: */wol {interface} {hwadddress}*"
 else
 	/usr/bin/etherwake -D -i "$WOLIFACE" "$HWADDR"
