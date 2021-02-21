e6845b3dfc772d7b3b5fcb6ea80eb61b09786e6e
Konstantine Shevlakov <konst.shell@gmail.com>
2021-02-21 07:22:40 -0500

    telegrambot: opkg: add list-installed action

--- root/usr/lib/telegrambot/plugins/opkg.sh
+++ root/usr/lib/telegrambot/plugins/opkg.sh
@@ -34,6 +34,24 @@ remove_(){
 	MSG="Package(s): $PKG removed."
 }
 
+installed_(){
+	PKGS="$(opkg list-installed | awk '{print $1}')"
+	#PKGS="Test TEst2 TEst3"
+	MSG="Installed package(s):\n\`\`\`\n$(echo ${PKGS})\`\`\`"
+}
+
+help_(){
+	MSG="Usage: */opkg command [argument]*\n\
+	Aviable opkg commands:\n\
+	\t\tupdate - update packages list\n\
+	\t\tinstall pkgname - install packages (max 5 package names)\n\
+	\t\tremove pkgname - remove packages (max 5 package names)\n\
+	\t\tlist-installed [pkgname] - list installed packages [package]\n\
+	\t\tlist-upgrade - list upgradabe packages\n\
+	\t\trun-upgrade - run upgrade upgradable packages\n\
+	\t\thelp - this help."
+}
+
 case ${ACTION} in
 	update)
 		update_
@@ -55,5 +73,12 @@ case ${ACTION} in
 		remove_
 		echo -en "$MSG"
 	;;
-	*) echo -en " Usage: */opkg {update|install|remove|list-upgrade|run-upgrade} [package(s) max 5]*" ;;
+	list-installed)
+		installed_
+		echo -en "$MSG"
+	;;
+	*)
+		help_
+		echo -en "$MSG"
+	;;
 esac
