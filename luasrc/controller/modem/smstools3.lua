f4de5dad738ded70f81a2d9b0e0b93588c6b35e8
koshev-msk <66990040+koshev-msk@users.noreply.github.com>
2021-12-04 21:00:11 +0300

    Fix login menu OpenWrt 21.

--- luasrc/controller/modem/smstools3.lua
+++ luasrc/controller/modem/smstools3.lua
@@ -5,13 +5,13 @@ module("luci.controller.modem.smstools3", package.seeall)
 local utl = require "luci.util"
 
 function index()
-	entry({"admin", "modem"},  firstchild(), "Modem", 45).dependent = false
-	entry({"admin", "modem", "sms"}, alias ("admin", "modem", "sms", "in_sms"), translate("Smstools3 SMS"), 11)
-	entry({"admin", "modem", "sms", "in_sms"}, template("modem/sms/in"), translate("Incoming"), 22)
-	entry({"admin", "modem", "sms", "out_sms"}, template("modem/sms/out"), translate("Outcoming"),23)
-	entry({"admin", "modem", "sms", "send_sms"}, template("modem/sms/send"), translate("Push"), 24)
-	entry({"admin", "modem", "sms", "setup_sms"}, cbi("modem/smstools3"), translate("Setup"), 25)
-	entry({"admin", "modem", "sms", "event"}, form("modem/smsevent"), translate("User Script"), 26)
+	entry({"admin", "modem"},  firstchild(), "Modem", 45).acl_depends={"unauthenticated"}
+	entry({"admin", "modem", "sms"}, alias ("admin", "modem", "sms", "in_sms"), translate("Smstools3 SMS"), 11).acl_depends={"unauthenticated"}
+	entry({"admin", "modem", "sms", "in_sms"}, template("modem/sms/in"), translate("Incoming"), 22).acl_depends={"unauthenticated"}
+	entry({"admin", "modem", "sms", "out_sms"}, template("modem/sms/out"), translate("Outcoming"),23).acl_depends={"unauthenticated"}
+	entry({"admin", "modem", "sms", "send_sms"}, template("modem/sms/send"), translate("Push"), 24).acl_depends={"unauthenticated"}
+	entry({"admin", "modem", "sms", "setup_sms"}, cbi("modem/smstools3"), translate("Setup"), 25).acl_depends={"unauthenticated"}
+	entry({"admin", "modem", "sms", "event"}, form("modem/smsevent"), translate("User Script"), 26).acl_depends={"unauthenticated"}
 	entry({"admin", "modem", "push_sms"}, call("action_send_sms"))
 	entry({"admin", "modem", "erase_in_sms"}, call("action_in_erase_sms"))
 	entry({"admin", "modem", "erase_out_sms"}, call("action_out_erase_sms"))
