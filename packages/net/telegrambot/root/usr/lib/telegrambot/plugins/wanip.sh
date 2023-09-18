998bb5ef61386255d9a41d532724c49824f2adb6
sid <konst.shell@gmail.com>
2023-09-18 14:24:03 +0300

    telegrambot: make /wanip answer to whois url

--- packages/net/telegrambot/root/usr/lib/telegrambot/plugins/wanip.sh
+++ packages/net/telegrambot/root/usr/lib/telegrambot/plugins/wanip.sh
@@ -1,2 +1,3 @@
 #ip r | awk '/default/{print "interface "$5": "$9}'
-nslookup myip.opendns.com resolver1.opendns.com | awk '/^Address( 1)?: / { print $3 }'
+IP=$(nslookup myip.opendns.com resolver1.opendns.com | awk '/^Address( 1)?: / { print $3 }')
+echo -en "[$IP](https://whois.ru/$IP)"
