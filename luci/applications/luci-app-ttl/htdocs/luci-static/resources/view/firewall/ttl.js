785bc0e61a00c36191d878254349e589028a3e58
sid <konst.shell@gmail.com>
2023-10-11 21:55:11 +0300

    luci-app-ttl: add package

---
+++ luci/applications/luci-app-ttl/htdocs/luci-static/resources/view/firewall/ttl.js
@@ -0,0 +1,110 @@
+'use strict';
+'require view';
+'require ui';
+'require rpc';
+'require uci';
+'require form';
+'require fs';
+'require network';
+'require firewall as fwmodel';
+'require tools.firewall as fwtool';
+'require tools.widgets as widgets';
+
+var briefInfo = _('Firewall restart required. <br>In Method proxy Proxy server must be configured in transparent mode on port 3128 tcp.<br>Disable masquerade recommened.');
+
+var rebootButton = E('button', {
+        'class': 'btn cbi-button cbi-button-neutral',
+        'click': ui.createHandlerFn(this, function() {
+       	        return handleAction('fwrestart');
+        }),
+}, _('Restart'));
+
+var FWrestart = form.DummyValue.extend({
+        load: function() {
+       	        var setupButton = E('button', {
+               	                'class': 'cbi-button cbi-button-neutral',
+                                'click': ui.createHandlerFn(this, function() {
+       	                                                return handleAction('reload');
+               	                                }),
+                        }, _('Restart Firewall'));
+       	        return L.resolveDefault(fs.exec_direct('/etc/init.d/firewall'), ['restart']).then(L.bind(function(html) {
+               	        this.default = E([
+                       	        E('div', { 'class': 'cbi-value' }, [
+                               	                E('label', { 'class': 'cbi-value-title' },
+                                       	                _('Restart Firewall')
+                                                ),
+       	                                        E('div', { 'class': 'cbi-value-field', 'style': 'width:25vw' },
+               	                                                E('div', { 'class': 'cbi-section-node' }, [
+                       	                                                rebootButton,
+                               	                                ]),
+                                                ),
+       	                                ]),
+       	                        ]);
+               	}, this));
+        }
+});
+
+function handleAction(ev) {
+	if (ev === 'fwrestart') {
+		fs.exec('/etc/init.d/firewall', ['restart']);
+	}
+}
+
+return view.extend({
+	render: function() {
+		var m, s, o;
+
+		m = new form.Map('ttl', _('Antitetheting Config'),
+			briefInfo);
+
+		s = m.section(form.TypedSection, 'fw');
+		s.anonymous = true;
+		o = s.option(FWrestart);
+
+		s = m.section(form.TypedSection, 'ttl', _('TTL or Proxy antitether'));
+		s.anonymous = true;
+		s.addremove = true;
+
+		o = s.option(widgets.NetworkSelect, 'iface', _('Set interface'));
+		o.exclude = s.section;
+		o.nocreate = true;
+		o.optional = true;
+
+		o = s.option(form.ListValue, 'method', _('Method'),
+			_('TTL method outgoing interface<br />Proxy method incoming interfcace'));
+		o.value('ttl', 'TTL');
+		o.value('proxy', 'Proxy');
+
+		o = s.option(form.Flag, 'advanced', _('Advanced Option'));
+		o.default = '1';
+		o.rmempty = false;
+
+		o = s.option(form.ListValue, 'inet', _('Inet Family'));
+		o.value('ipv4', 'IPv4');
+		o.value('ipv6', 'IPv6');
+		o.value('ipv4v6', _('Both'));
+		o.rmempty = true;
+		o.editable = true;
+		o.depends('advanced', '1');
+
+		o = s.option(form.Value, 'ttl', _('TTL Value'),
+			_('Select TTL value. Range 1 - 255'));
+		o.value('64','64')
+		o.value('128','128')
+		o.default = '64';
+		o.rmempty = true;
+		o.editable = true;
+		o.depends({advanced: '1', method: /ttl/});
+
+		o = s.option(form.Value, 'ports', _('Ports'),
+			_('Incoming ports route to proxy-server<br />Custom ports range: 0-65535'));
+		o.editable = true;
+		o.rmempty = true;
+		o.value('all', _('ALL Ports'));
+		o.value('http', _('HTTP Ports'));
+		o.default = 'all';
+		o.depends({advanced: '1', method: /proxy/})
+
+		return m.render();
+	},
+});
---
+++ luci/applications/luci-app-ttl/root/usr/share/ttl.sh
@@ -0,0 +1,110 @@
+
+SECTIONS=$(echo $(uci show ttl | awk -F [\]\[\@=] '/=ttl/{print $3}'))
+
+get_vars(){
+	for v in method advanced inet ports ttl iface; do
+		eval $v=$(uci -q get ttl.@ttl[${s}].${v} 2>/dev/nul)
+	done
+}
+
+
+
+method_ttl(){
+	if [ ! $ttl ]; then
+		ttl=64
+	fi
+	case $(($ttl % 2)) in
+		0) TTL_INC=4 ;;
+		*) TTL_INC=5 ;;
+	esac
+	for T in $IPT; do
+		case $T in
+			iptables)
+				SUFFIX="TTL --ttl-set"
+				if [ $iface ]; then
+					$T -t mangle -A PREROUTING -i $DEV -m ttl --ttl 1 -j TTL --ttl-inc $TTL_INC
+				else
+					$T -t mangle -A PREROUTING -m ttl --ttl 1 -j TTL --ttl-inc $TTL_INC
+				fi
+			;;
+			ip6tables)
+				SUFFIX="HL --hl-set"
+				if [ $iface ]; then
+					$T -t mangle -A PREROUTING -i $DEV -m hl --hl 1 -j HL --hl-inc $TTL_INC
+					$T -t mangle -A OUTPUT -o $DEV -j $SUFFIX $ttl
+					$T -t mangle -A POSTROUTING -o $DEV -j $SUFFIX $ttl
+				else
+					$T -t mangle -A PREROUTING -m hl --hl 1 -j HL --hl-inc $TTL_INC
+				fi
+			;;
+		esac
+		if [ $iface ]; then
+			$T -t mangle -A OUTPUT -o $DEV -j $SUFFIX $ttl
+			$T -t mangle -A POSTROUTING -o $DEV -j $SUFFIX $ttl
+		else
+			$T -t mangle -A OUTPUT -j $SUFFIX $ttl
+			$T -t mangle -A POSTROUTING -j $SUFFIX $ttl
+		fi
+	done				
+}
+
+
+method_proxy(){
+	for T in $IPT; do
+		case $T in
+			iptables)
+				IPADDR=$(ifstatus $iface | jsonfilter -e '@["ipv4-address"][*]["address"]')
+				END="${IPADDR}:3128"
+			;;
+			ip6tables)
+				for a in $(ifstatus $iface | jsonfilter -e '@["ipv6-prefix-assignment"][*]["local-address"]["address"]'); do
+					IPADDR="$a"
+				done
+				END="[$IPADDR]:3128"
+			;;
+		esac
+		$T -t nat -N PROXY
+		$T -t nat -A PREROUTING -i $DEV -j PROXY
+		case $ports in
+			all)
+				$T -t nat -A PROXY ! -d ${IPADDR} \
+					! -s ${IPADDR} -p tcp \
+					-j DNAT --to-destination $END
+			;;
+			http)
+				$T -t nat -A PROXY ! -d ${IPADDR} \
+					! -s ${IPADDR} -p tcp -m multiport \
+					--dports 80,443 -j DNAT --to-destination $END
+			;;
+			*)
+				if [ $ports ]; then
+					$T -t nat -A PROXY ! -d ${IPADDR} \
+						! -s ${IPADDR} -p tcp -m multiport \
+						--dports $ports -j DNAT --to-destination $END
+				else
+					$T -t nat -A PROXY ! -d ${IPADDR} \
+						! -s ${IPADDR} -p tcp \
+						-j DNAT --to-destination $END
+				fi
+			;;
+		esac
+	done
+}	
+
+for s in $SECTIONS; do
+	if [ "$s" ]; then
+		get_vars
+	else
+		exit 0
+	fi
+	case $inet in
+		ipv4) IPT="iptables" ;;
+		ipv6) IPT="ip6tables" ;;
+		*) IPT="iptables ip6tables";;
+	esac
+	DEV=$(ifstatus $iface | jsonfilter -e '@["device"]')
+	case $method in
+		ttl) method_ttl ;;
+		proxy) method_proxy ;;
+	esac
+done
