c9369ebcdd1cf09768319ff473914e0730c340b2
sid <konst.shell@gmail.com>
2023-12-01 19:58:43 +0300

    luci-app-telegrambot: move codebase from Lua to JavaScript

--- luci/applications/luci-app-telegrambot/luasrc/controller/telegrambot.lua
+++
@@ -1,8 +0,0 @@
-module("luci.controller.telegrambot", package.seeall)
-
-function index()
-        if not nixio.fs.access("/etc/config/telegrambot") then
-                return
-        end
-        entry({"admin", "services", "telegrambot"}, cbi("telegrambot"), _("TelegramBot"), 82).acl_depends={"unauthenticated"}
-end
