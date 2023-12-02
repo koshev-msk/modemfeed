614f08caeceb1156e08ac2fd32047623776e93e4
sid <konst.shell@gmail.com>
2023-12-02 21:19:38 +0300

    luci-app-3proxy: move codebase from Lua to JavaScript

--- luci/applications/luci-app-3proxy/luasrc/controller/3proxy.lua
+++
@@ -1,7 +0,0 @@
-module("luci.controller.3proxy", package.seeall)
-
-function index()
-	entry({"admin", "services", "3proxy"}, alias("admin", "services", "3proxy", "config"),  _("3proxy"), 93).acl_depends={"unauthenticated"}
-	entry({"admin", "services", "3proxy", "config"}, cbi("3proxy/3proxy"), _("Setup"), 94).acl_depends={"unauthenticated"}
-	entry({"admin", "services", "3proxy", "template"}, form("3proxy/3proxy_tpl"), _("Edit Config File"), 95).acl_depends={"unauthenticated"}
-end
