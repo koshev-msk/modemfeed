aae641ca668e9483cdc7c495d3ccb33d107d1153
prusa-dev <prusa@mail.ru>
2024-03-04 16:07:02 +0300

    fm350-modem: Serial port initialization

--- packages/net/fm350-modem/root/etc/hotplug.d/usb/01_fm350.sh
+++ packages/net/fm350-modem/root/etc/hotplug.d/usb/01_fm350.sh
@@ -16,5 +16,13 @@ find_fm350_iface() {
 	fi
 }
 
+if [ "$PRODUCT" = 'e8d/7126/1' ] && [ "$ACTION" = add ]; then
+    echo "0e8d 7126 ff" > /sys/bus/usb-serial/drivers/option1/new_id
+fi
+
+if [ "$PRODUCT" = 'e8d/7127/1' ] && [ "$ACTION" = add ]; then
+    echo "0e8d 7127 ff" > /sys/bus/usb-serial/drivers/option1/new_id
+fi
+
 config_load network
 config_foreach find_fm350_iface interface
