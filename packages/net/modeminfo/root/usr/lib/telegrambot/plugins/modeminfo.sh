f26d32d12edc7a92b3d63479b513f7e9a4327da5
koshev-msk <konst.shell@gmail.com>
2024-03-27 14:01:38 +0300

    modeminfo: telegram plugin
    	* add inital multiple modems support

--- packages/net/modeminfo/root/usr/lib/telegrambot/plugins/modeminfo.sh
+++ packages/net/modeminfo/root/usr/lib/telegrambot/plugins/modeminfo.sh
@@ -1,53 +1,58 @@
 #!/bin/sh
-. /usr/share/libubox/jshn.sh
+DATA=$(/usr/share/modeminfo/modeminfo)
+SECTIONS=$(echo $(uci show modeminfo | awk -F [\]\[\@=] '/=modeminfo/{print $3}'))
 
-json_load "$(/usr/bin/modeminfo)"
-json_get_var DEVICE device
-json_get_var COPS cops
-json_get_var MODE mode
-json_get_var PERS csq_per
-echo -ne "*Device:* $DEVICE\n*Operator:* $COPS 📶${PERS}% $MODE\n"
-json_get_var CH arfcn
+# load channel to band converter
 . /usr/share/modeminfo/scripts/ch_to_band
-ch_to_band $CH
-json_get_var RSSI rssi
-case $MODE in
-	LTE)
-		json_get_var CA lteca
-		if [ $CA -ge 1 ]; then
-		        json_get_var SB scc
-		        CA="$(($CA+1))xCA"
-		        BAND="${SC}${SB}"
-		        echo -ne "*CA:* $CA BAND ($BAND)\n"
-		else
-		        echo -ne "*BAND:* $SC\n"
-		fi
-		json_get_var RSRP rsrp
-		json_get_var RSRQ rsrq
-		json_get_var SINR sinr
-		json_get_var ENBID enbid
-		json_get_var PCI pci
-		json_get_var CELL cell
-        	json_get_var DIST distance
-		echo -ne "*RSSI/RSRP:* ${RSSI}dBm/${RSRP}dBm\n*RSRQ/SINR:* ${RSRQ}dB/${SINR}dB\n*eNBID-Cell/PCI:* ${ENBID}-${CELL}/${PCI}"
-                if [ $DIST ] && [ "$DIST" -ne "0.00" ]; then
-                        echo -ne " ~${DIST}km\n"
-                else
-                        echo -ne "\n"
-                fi
-	;;
-	UMT*|WCD*|*HSP*|*HUS*)
-		json_get_var SINR sinr
-		json_get_var LAC lac
-		json_get_var CID cid
-		echo -ne "*BAND:* $SC\n"
-		echo -ne "*RSSI/ECIO:* ${RSSI}dBm/${SINR}dB\n*LAC/CID:* ${LAC}/${CID}"
-	;;
-	*)
-		json_get_var LAC lac
-		json_get_var CID cid
-		echo -ne "*BAND:* $SC\n"
-		echo -ne "*RSSI:*  ${RSSI}dBm\n*LAC/CID:* ${LAC}/${CID}"
-	;;
-esac
 
+get_data(){
+	jsonfilter -s "$DATA" -e "MODE=$['modem'][$s]['mode']" \
+		-e "DEVICE=$['modem'][$s]['device']" \
+		-e "CH=$['modem'][$s]['arfcn']" \
+		-e "COPS=$['modem'][$s]['cops']" \
+		-e "PERS=$['modem'][$s]['csq_per']" \
+		-e "CA=$['modem'][$s]['lteca']" \
+		-e "RSSI=$['modem'][$s]['rssi']" \
+		-e "RSRP=$['modem'][$s]['rsrp']" \
+		-e "RSRQ=$['modem'][$s]['rsrq']" \
+		-e "SINR=$['modem'][$s]['sinr']" \
+		-e "ENBID=$['modem'][$s]['enbid']" \
+		-e "PCI=$['modem'][$s]['pci']" \
+		-e "CELL=$['modem'][$s]['cell']" \
+		-e "DIST=$['modem'][$s]['distance']" \
+		-e "SB=$['modem'][$s]['scc']"
+}
+
+for s in ${SECTIONS}; do
+	eval $(echo "$(get_data)" | sed -e 's/export//g')
+	ch_to_band $CH
+	echo -ne "*Modem $(($s+1))*\n"
+	echo -ne "*Device:* $DEVICE\n*Operator:* $COPS 📶${PERS}% $MODE\n"
+	case $MODE in
+		LTE)
+			if [ $CA -ge 1 ]; then
+				CA="$(($CA+1))xCA"
+				BAND="${SC}${SB}"
+				echo -ne "*CA:* $CA BAND ($BAND)\n"
+			else
+				echo -ne "*BAND:* $SC\n"
+			fi
+			echo -ne "*RSSI/RSRP:* ${RSSI}dBm/${RSRP}dBm\n*RSRQ/SINR:* ${RSRQ}dB/${SINR}dB\n*eNBID-Cell/PCI:* ${ENBID}-${CELL}/${PCI}"
+			if [ "$DIST" ] && [ "$DIST" != "0.00" ]; then
+				echo -ne " ~${DIST}km\n"
+			else
+				echo -ne "\n"
+			fi
+		;;
+		UMT*|WCD*|*HSP*|*HUS*)
+			echo -ne "*BAND:* $SC\n"
+			echo -ne "*RSSI/ECIO:* ${RSSI}dBm/${SINR}dB\n*LAC/CID:* ${LAC}/${CID}"
+		;;
+		*)
+			echo -ne "*BAND:* $SC\n"
+			echo -ne "*RSSI:*  ${RSSI}dBm\n*LAC/CID:* ${LAC}/${CID}"
+		;;
+	esac
+			
+	echo -e "\n"
+done
