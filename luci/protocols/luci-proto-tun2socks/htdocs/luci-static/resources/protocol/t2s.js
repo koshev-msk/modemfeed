c4ed7b2e8f95df6e66436af29fff6a6e365936d4
koshev-msk <konst.shell@gmail.com>
2024-10-16 15:05:04 +0300

    tun2socks: update netifd scripts
    	* add support shadowsocks proxy
    	* update luci proto
    
    	TODO:
    	* base64 encryption ss proxy
    	* unix domain socket support (use option opts present)

--- luci/protocols/luci-proto-tun2socks/htdocs/luci-static/resources/protocol/t2s.js
+++ luci/protocols/luci-proto-tun2socks/htdocs/luci-static/resources/protocol/t2s.js
@@ -38,50 +38,87 @@ return network.registerProtocol('t2s', {
 	renderFormOptions: function(s) {
 		var dev = this.getL3Device() || this.getDevice(), o;
 
+                o = s.taboption('general', form.ListValue, 'proxy', _('Proxy Type'));
+		o.value('http', 'HTTP');
+                o.value('socks4', 'SOCKS4');
+                o.value('socks5', 'SOCKS5');
+                o.value('ss', 'Shadowsocks');
+                o.value('relay', _('Relay'));
+                o.value('direct', _('Direct'));
+                o.value('reject', _('Reject'));
+		o.default = 'socks5';
+                o.rmempty = true;
+		
+		// TODO
+		//o = s.taboption('general', form.Flag, 'socket', _('Use Socket'), _('Use Unix Domain Socket instead address'));
+		//o.depends({'proxy': 'socks5' });
+		//o.rmempty = true;
+
 		o = s.taboption('general', form.Value, 'ipaddr', _('IPv4 Address'));
 		o.datatype = 'ip4addr("nomask")';
+		o.depends({'proxy': /http|socks4|socks5|relay|ss/ });
 		o.rmempty = false;
 
 		o = s.taboption('general', form.Value, 'netmask', _('IPv4 Netmask'));
 		o.value('255.255.255.0', '255.255.255.0');
 		o.value('255.255.0.0', '255.255.0.0');
-		
+		o.depends({'proxy': /http|socks4|socks5|relay|ss/ });
 		o.rmempty = false;
 
 		o = s.taboption('general', form.Value, 'gateway', _('IPv4 Gateway'));
 		o.datatype = 'ip4addr("nomask")';
+		o.depends({'proxy': /http|socks4|socks5|relay|ss/ });
 		o.rmempty = false;
 
-		o = s.taboption('general', form.ListValue, 'proxy', _('Proxy Type'));
-		o.value('socks4', 'SOCKS4');
-		o.value('socks5', 'SOCKS5');
-		o.value('http', 'HTTP');
-		o.value('ss', 'Shadowsocks');
-		o.value('relay', _('Relay'));
-		o.value('direct', _('Direct'));
-		o.value('reject', _('Reject'));
-		o.rmempty = true;
-
-		o = s.taboption('general', form.Value, 'host', _('Proxy Address'));
+		o = s.taboption('general', form.Value, 'host', _('Proxy Address'), _('IP-address or FQDN hostname proxy'));
 		o.datatype = 'or(hostname,ip4addr("nomask"))';
-		o.rmempty = false;
+		o.depends({'proxy': /http|socks4|socks5|relay|ss/ });
+		o.rmempty = true;
 
 		o = s.taboption('general', form.Value, 'port', _('Proxy PORT'));
 		o.datatype = 'range(1, 65535)';
-		o.rmempty = false;
+		o.depends({'proxy': /http|socks4|socks5|relay|ss/ });
+		o.rmempty = true;
 
 		o = s.taboption('general', form.Flag, 'advanced', _('Autentification'), _('Authentification and encryption.'));
+		o.depends({'proxy': /socks4|socks5|relay|ss/ });
 		o.rmempty = true;
 
 		o = s.taboption('general', form.Value, 'username', _('Proxy USER'));
-		o.depends('advanced', '1');
-		
+		o.depends({'advanced': '1', 'proxy': /socks|relay/});
+
+		o = s.taboption('general', form.ListValue, 'encrypt', _('Encryption'));
+		o.value('none','none');
+		o.value('table','table');
+		o.value('rc4','rc4');
+		o.value('rc4-md5','rc4-md5');
+		o.value('aes-128-cfb','aes-128-cfb');
+		o.value('aes-192-cfb','aes-192-cfb');
+		o.value('aes-256-cfb','aes-256-cfb');
+		o.value('aes-128-ctr','aes-128-ctr');
+		o.value('aes-192-ctr','aes-192-ctr');
+		o.value('aes-256-ctr','aes-256-ctr');
+		o.value('aes-128-gcm','aes-128-gcm');
+		o.value('aes-192-gcm','aes-192-gcm');
+		o.value('aes-256-gcm','aes-256-gcm');
+		o.value('camellia-128-cfb','camellia-128-cfb');
+		o.value('camellia-192-cfb','camellia-192-cfb');
+		o.value('camellia-256-cfb','camellia-256-cfb');
+		o.value('bf-cfb','bf-cfb');
+		o.value('salsa20','salsa20');
+		o.value('chacha20','chacha20');
+		o.value('chacha20-ietf','chacha20-ietf');
+		o.value('chacha20-ietf-poly1305','chacha20-ietf-poly1305');
+		o.value('xchacha20-ietf-poly1305','xchacha20-ietf-poly1305');
+		o.depends({advanced: '1', proxy: 'ss'});
+
 		o = s.taboption('general', form.Value, 'password', _('Proxy Password'));
 		o.password = true;
-		o.depends('advanced', '1');
-
-		o = s.taboption('general', form.Value, 'encrypt', _('Encryption'), _('Example: aes-256-gcm'));
-		o.depends('advanced', '1');
+		o.depends({'advanced': '1', 'proxy': /socks5|relay|ss/ });
+		// TODO
+		//o = s.taboption('general',form.Flag, 'base64enc', _('Encrypt base64'));
+		//o.depends({'advanced': '1', 'proxy': 'ss' });
+		//o.rmempty = true;
 
 		o = s.taboption('advanced', form.Value, 'mtu', _('Set MTU'), _('Set device maximum transmission unit'));
 		o.placeholder = dev ? (dev.getMTU() || '1500') : '1500';
--- packages/net/tun2socks/root/lib/netifd/proto/t2s.sh
+++ packages/net/tun2socks/root/lib/netifd/proto/t2s.sh
@@ -15,40 +15,125 @@ proto_t2s_init_config(){
 	proto_config_add_string "proxy"
 	proto_config_add_string "encrypt"
 	proto_config_add_string "loglevel"
-	proto_config_add_string "fwmark"
 	proto_config_add_string "obfs_host"
-	proto_config_add_int "mtu"
-	proto_config_add_int "port"
 	proto_config_add_string "username"
 	proto_config_add_string "password"
 	proto_config_add_string "opts"
+	proto_config_add_string "testing"
+	proto_config_add_int "mtu"
+	proto_config_add_int "port"
+	proto_config_add_int "fwmark"
+	proto_config_add_bool "socket"
+	proto_config_add_bool "base64enc"
 	proto_config_add_defaults
+}
+
+check_encrypt(){
+	case $encrypt in
+		"none"|\
+		"table"|\
+		"rc4"|\
+		"rc4-md5"|\
+		"aes-128-cfb"|\
+		"aes-192-cfb"|\
+		"aes-256-cfb"|\
+		"aes-128-ctr"|\
+		"aes-192-ctr"|\
+		"aes-256-ctr"|\
+		"aes-128-gcm"|\
+		"aes-192-gcm"|\
+		"aes-256-gcm"|\
+		"camellia-128-cfb"|\
+		"camellia-192-cfb"|\
+		"camellia-256-cfb"|\
+		"bf-cfb"|\
+		"salsa20"|\
+		"chacha20"|\
+		"chacha20-ietf"|\
+		"chacha20-ietf-poly1305"|\
+		"xchacha20-ietf-poly1305")
+				continue
+		;;
+		*)
+			proto_notify_error "$interface" WRONG_ENCRYPT_METHOD
+			proto_set_available "$interface" 0
+		;;
+	esac
 }
 
 proto_t2s_setup(){
 	local interface="$1"
-	local ifname ipaddr netmask gateway host proxy encrypt loglevel fwmark obfs_host port mtu username password opts $PROTO_DEFAULT_OPTIONS
-	json_get_vars ifname ipaddr netmask gateway host proxy encrypt loglevel obfs_host fwmark port mtu username password opts $PROTO_DEFAULT_OPTIONS
+	local ifname ipaddr netmask gateway host proxy encrypt loglevel fwmark 
+	local base64enc socket obfs_host port mtu
+	local username password opts $PROTO_DEFAULT_OPTIONS
+	json_get_vars ifname ipaddr netmask gateway host proxy encrypt loglevel fwmark
+	json_get_vars base64enc socket obfs_host port mtu
+	json_get_vars username password opts $PROTO_DEFAULT_OPTIONS
 	ifname=$interface
 	[ "$metric" = "" ] && metric="0"
 	[ "$proxy" = "" ] && proxy=socks5
 
 	[ "$host" -a "$port" ] && {
-		ARGS="-proxy ${proxy}://${host}:${port}"
-	}
-	[ "$host" -a "$port" -a "$username" -a "$password" ] && {
-		ARGS="-proxy ${proxy}://${username}:${password}@${host}:${port}"
-	}
-	[ "$host" -a "$port" -a "$username" -a "$password" -a "$encrypt" ] && {
-		ARGS="-proxy ${proxy}://${encrypt}:${username}:${password}@${host}:${port}"
+		case "$proxy" in
+			http) ARGS="-proxy ${proxy}://${host}:${port}" ;;
+			socks4)
+				[ "$username" ] && {
+					ARGS="-proxy ${proxy}://${username}@${host}:${port}"
+				} || {
+					ARGS="-proxy ${proxy}://${host}:${port}"
+				}
+			;;
+			socks5)
+				[ "$username" -a "$password" ] && {
+					ARGS="-proxy ${proxy}://${username}:${password}@${host}:${port}"
+				} || {
+					ARGS="-proxy ${proxy}://${host}:${port}"
+				}
+			;;
+			ss)
+				#check_encrypt
+				[ "$encrypt" -a "$password" ] && {
+					# TODO 
+					#[ "$base64enc" = "1" ] && {
+					#	[ "$obfs_host" ] && {
+					#		ARGS="-proxy ${proxy}://base64_encode(${encrypt}:${password})@${host}:${port}/\<\?obfs=http\;obfs-host=$obfs_host\>"
+					#	} || {
+					#		ARGS="-proxy ${proxy}://base64_encode(${encrypt}:${password})@${host}:${port}"
+					#	}
+					#} || {
+						[ "$obfs_host" ] && {
+							ARGS="-proxy ${proxy}://${encrypt}:${password}@${host}:${port}/\<\?obfs=http\;obfs-host=$obfs_host\>"
+						} || {
+							ARGS="-proxy ${proxy}://${encrypt}:${password}@${host}:${port}"
+						}
+					#}
+				} || {
+					ARGS="-proxy ${proxy}://${host}:${port}"
+				}
+			;;
+			relay)
+				[ "$username" -a "$password" ] && {
+					ARGS="-proxy ${proxy}://${encrypt}:${password}@${host}:${port}/\<nodelay=false\>"
+				} || {
+					ARGS="-proxy ${proxy}://${host}:${port}/\<nodelay=false\>"
+				}
+			;;
+		esac
 	}
 	
 	case $proxy in
 		direct|reject) ARGS="-proxy ${proxy}://" ;;
+		# TODO
+		#socks5)
+		#	[ "$socket" ] && {
+		#		ARGS="-proxy ${proxy}://${socket}"
+		#	}
+		#;;
 	esac
 
 	[ "x${ARGS}" = "x" ] && {
 		proto_notify_error "$interface" CONFIGURE_FAILED
+		proto_set_available "$interface" 0
 	}
 
 	[ "$loglevel" = "" ] && loglevel=error && {
@@ -59,12 +144,12 @@ proto_t2s_setup(){
 		ARGS="$ARGS -fwmark $fwmark"
 	}
 
-	[ "$opts" ] && {
-		 ARGS="$ARGS $OPTS"
-	}
-
 	[ "$mtu" ] && {
 		ARGS="$ARGS -mtu $mtu"
+	}
+
+	[ "$opts" ] && {
+		 ARGS="$ARGS $opts"
 	}
 
 	proto_init_update "$interface" 1
@@ -82,10 +167,12 @@ proto_t2s_setup(){
 	proto_send_update "$interface"
 	proto_run_command "$interface" /usr/sbin/tun2socks \
 		-device "$interface" $ARGS
+	set > /tmp/proto_up.log
 }
 
 proto_t2s_teardown(){
 	local interface="$1"
+	set > /tmp/proto_down.log
 	proto_kill_command "$interface"
 	ip tuntap del mode tun dev $interface
 }
