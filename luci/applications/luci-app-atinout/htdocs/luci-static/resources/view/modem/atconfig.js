7ff94bc1ac1dc9605724d055a0c484d5719a2df5
koshev-msk <konst.shell@gmail.com>
2025-11-05 11:05:40 +0300

    luci-app-atinout: fix save list button

--- luci/applications/luci-app-atinout/htdocs/luci-static/resources/view/modem/atconfig.js
+++ luci/applications/luci-app-atinout/htdocs/luci-static/resources/view/modem/atconfig.js
@@ -5,61 +5,67 @@
 'require rpc';
 
 /*
-	Copyright 2022-2023 Rafał Wabik - IceG - From eko.one.pl forum
+        Copyright 2022-2023 Rafał Wabik - IceG - From eko.one.pl forum
 
-	Modified for atinout by Konstantine Shevlakov <shevlakov@132lan.ru> 2023
-	
-	Licensed to the GNU General Public License v3.0.
+        Modified for atinout by Konstantine Shevlakov <shevlakov@132lan.ru> 2023
+
+        Licensed to the GNU General Public License v3.0.
 */
 
-var cmddesc = _("Each line must have the following format: 'At command description;AT command'. For user convenience, the file is saved to the location <code>/etc/atcommands.user</code>."); 
+var cmddesc = _("Each line must have the following format: <code>AT command description;AT+COMMAND</code>.<br />For user convenience, the file is saved to the location <code>/etc/atcommands.user</code>.");
 
 return view.extend({
-	load: function() {
-		return fs.read('/etc/atcommands.user').catch(function(err) {
-			return '';
-		});
-	},
+        load: function() {
+                return fs.read('/etc/atcommands.user').catch(function(err) {
+                        return '';
+                });
+        },
 
-	render: function(content) {
-		var content = content || '';
-		
-		return E('div', { 'class': 'cbi-map' }, [
-			E('div', { 'class': 'cbi-map-descr' }, _('AT Commands Configuration')),
-			E('div', { 'class': 'cbi-section' }, [
-				E('div', { 'class': 'cbi-section-descr' }, cmddesc),
-				E('div', { 'class': 'cbi-value' }, [
-					E('label', { 'class': 'cbi-value-title' }, _('User AT commands')),
-					E('div', { 'class': 'cbi-value-field' }, [
-						E('textarea', {
-							'class': 'cbi-input-textarea',
-							'rows': 20,
-							'style': 'width: 100%',
-							'name': 'atcommands'
-						}, content),
-						E('div', { 'class': 'cbi-button' }, [
-							E('button', {
-								'class': 'cbi-button cbi-button-save',
-								'click': ui.createHandlerFn(this, 'saveCommands')
-							}, _('Save'))
-						])
-					])
-				])
-			])
-		]);
-	},
+        render: function(content) {
+                var content = content || '';
 
-	saveCommands: function(ev) {
-		var textarea = document.querySelector('textarea[name="atcommands"]');
-		var commands = textarea.value.trim().replace(/\r\n/g, '\n') + '\n';
-		
-		return fs.write('/etc/atcommands.user', commands).then(function() {
-			ui.addNotification(null, E('p', _('AT commands saved successfully')), 'info');
-		}).catch(function(err) {
-			ui.addNotification(null, E('p', _('Error saving AT commands: ') + err.message), 'error');
-		});
-	},
+                return E('div', { 'class': 'cbi-map' }, [
+                        E('div', { 'class': 'cbi-map-descr' }, _('AT Commands Configuration')),
+                        E('div', { 'class': 'cbi-section' }, [
+                                E('div', { 'class': 'cbi-section-descr' }, cmddesc),
+                                E('div', { 'class': 'cbi-value' }, [
+                                        E('label', { 'class': 'cbi-value-title' }, _('User AT commands')),
+                                        E('div', { 'class': 'cbi-value-field' }, [
+                                                E('textarea', {
+                                                        'class': 'cbi-input-textarea',
+                                                        'rows': 20,
+                                                        'style': 'width: 100%',
+                                                        'name': 'atcommands'
+                                                }, content)
+                                        ])
+                                ]),
+                                E('div', { 
+                                        'class': 'cbi-value',
+                                        'style': 'display: flex; justify-content: flex-end; margin-top: 20px;' 
+                                }, [
+                                        E('div', { 'class': 'cbi-value-field right' }, [
+                                                E('button', {
+                                                        'class': 'cbi-button cbi-button-save',
+                                                        'click': ui.createHandlerFn(this, 'saveCommands')
+                                                }, _('Save'))
+                                        ])
+                                ])
+                        ])
+                ]);
+        },
 
-	handleSave: null,
-	handleReset: null
+        saveCommands: function(ev) {
+                var textarea = document.querySelector('textarea[name="atcommands"]');
+                var commands = textarea.value.trim().replace(/\r\n/g, '\n') + '\n';
+
+                return fs.write('/etc/atcommands.user', commands).then(function() {
+                        ui.addNotification(null, E('p', _('AT commands list saved successfully')), 'info');
+                }).catch(function(err) {
+                        ui.addNotification(null, E('p', _('Error saving list: ') + err.message), 'error');
+                });
+        },
+
+        handleSaveApply: null,
+        handleSave: null,
+        handleReset: null
 });
