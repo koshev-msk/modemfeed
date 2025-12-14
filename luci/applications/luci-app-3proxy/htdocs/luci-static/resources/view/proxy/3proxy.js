146058003608479691a3d94296d6bf02799e47d8
koshev-msk <konst.shell@gmail.com>
2025-12-14 10:33:10 +0300

    luci-app-3proxy: bump to 0.1.1
    	* Remove System Buttons, add hot restart button and save.

--- luci/applications/luci-app-3proxy/htdocs/luci-static/resources/view/proxy/3proxy.js
+++ luci/applications/luci-app-3proxy/htdocs/luci-static/resources/view/proxy/3proxy.js
@@ -1,36 +1,111 @@
 'use strict';
-'require form';
 'require fs';
 'require view';
-'require uci';
 'require ui';
-'require tools.widgets as widgets'
+'require uci';
 
 
 return view.extend({
-	load: function(){
-		uci.load('3proxy');
+	load: function() {
+		return uci.load('3proxy');
 	},
 
-	render: function(data) {
-		var config = uci.sections('3proxy');
-		var file = (config.config);
-		var m, s, o;
+	render: function() {
+		var configPath = '/etc/3proxy.cfg';
+		
+		uci.sections('3proxy', '3proxy').forEach(function(s) {
+			if (s['.type'] === '3proxy' && s.config) {
+				configPath = s.config;
+			}
+		});
+		
+		this.configPath = configPath;
+		
+		var container = E('div', { 'class': 'cbi-map' }, [
+			E('h2', {}, _('3proxy Configuration')),
+			E('div', { 'class': 'cbi-section' }, [
+				E('div', { 'class': 'cbi-section-descr' }, [
+					_('Edit 3proxy configuration file.'),
+					E('br'),
+					_('Config file: '),
+					E('code', {}, configPath)
+				]),
+				E('div', { 'class': 'cbi-value' }, [
+					E('label', { 
+						'class': 'cbi-value-title',
+						'style': 'display: block; font-weight: bold;' 
+					}, _('Configuration')),
+					E('br'), E('br'),
+					E('textarea', {
+						'class': 'cbi-input-textarea',
+						'rows': 20,
+						'style': 'width: 100%; box-sizing: border-box;',
+						'name': 'config',
+						'id': 'config-textarea'
+					}, '')
+				]),
+				E('div', { 
+					'class': 'cbi-value',
+					'style': 'display: flex; justify-content: flex-end; margin-top: 20px; gap: 10px;' 
+				}, [
+					E('div', { 'class': 'cbi-value-field right' }, [
+						E('button', {
+							'class': 'cbi-button cbi-button-save',
+							'click': ui.createHandlerFn(this, 'saveConfig')
+						}, _('Save')),
+						' ',
+						E('button', {
+							'class': 'cbi-button cbi-button-apply',
+							'click': ui.createHandlerFn(this, 'restartService')
+						}, _('Restart 3proxy'))
+					])
+				])
+			])
+		]);
+		
+		var self = this;
+		fs.read(configPath).catch(function(err) {
+			return '';
+		}).then(function(content) {
+			var textarea = document.getElementById('config-textarea');
+			if (textarea) {
+				textarea.value = content || '';
+			}
+		});
+		
+		return container;
+	},
 
-		m = new form.Map('3proxy', _('Configuration'), _('Configuration file for 3proxy.'));
+	saveConfig: function() {
+		var textarea = document.querySelector('textarea[name="config"]');
+		if (!textarea) {
+			textarea = document.getElementById('config-textarea');
+		}
+		
+		var config = textarea.value.trim().replace(/\r\n/g, '\n') + '\n';
+		
+		var configPath = this.configPath || '/etc/3proxy.cfg';
 
-		s = m.section(form.TypedSection, '3proxy', null);
-		s.anonymous = true;
+		return fs.write(configPath, config).then(function() {
+			ui.addNotification(null, E('p', _('Configuration saved successfully')), 'info');
+		}).catch(function(err) {
+			ui.addNotification(null, E('p', _('Error saving configuration: ') + err.message), 'error');
+		});
+	},
 
-		o = s.option(form.TextValue, '_tmpl', _('Edit config 3proxy'));
-		o.rows = 20;
-		o.cfgvalue = function(section_id) {
-			return fs.trimmed('/etc/3proxy.cfg');
-		};
-		o.write = function(section_id, formvalue) {
-			return fs.write(('/etc/3proxy.cfg'), formvalue.trim().replace(/\r\n/g, '\n') + '\n');
-		};
+	restartService: function() {
+		return fs.exec('/etc/init.d/3proxy', ['restart']).then(function(res) {
+			if (res.code === 0) {
+				ui.addNotification(null, E('p', _('3proxy restarted successfully')), 'info');
+			} else {
+				ui.addNotification(null, E('p', _('Error restarting 3proxy: ') + res.stderr), 'error');
+			}
+		}).catch(function(err) {
+			ui.addNotification(null, E('p', _('Error restarting 3proxy: ') + err.message), 'error');
+		});
+	},
 
-		return m.render();
-	}
+	handleSaveApply: null,
+	handleSave: null,
+	handleReset: null
 });
